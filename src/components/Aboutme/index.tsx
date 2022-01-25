3db7debe3cb6d5c09397f7774659cf67eb064e1c
AndresLLogares <Andresl940@hotmail.com>
2022-01-25 16:32:48 -0300

    new loading

--- src/components/Aboutme/index.tsx
+++ src/components/Aboutme/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
